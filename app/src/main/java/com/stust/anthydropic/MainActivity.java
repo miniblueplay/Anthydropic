8a3c7ad730b5e289dfe9ea178e6b0bafdfd530f3
msg <774418855299663z@gmail.com>
2023-10-13 19:55:05 +0800

    完成藍芽連線與發送資料

--- app/src/main/java/com/stust/anthydropic/MainActivity.java
+++ app/src/main/java/com/stust/anthydropic/MainActivity.java
@@ -29,6 +29,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.stust.anthydropic.databinding.ActivityMainBinding;
@@ -65,10 +66,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //接收-------------------
     private static final int CHUNK_SIZE = 4096;
     //發送-------------------
-    private int bleSendText[] = {0,0,90,0,1,16};
+    private int bleSendText[] = {0,0,0,0,0,1};
+                                                // "小指,名指,中指,食指,拇指,始能"
     //******************************************
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //隱藏導航欄
+        hideNav();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         view = binding.getRoot();
         setContentView(view);
+
+        //隱藏導航欄
         hideNav();
 
         // 視圖初始化
@@ -85,10 +95,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 建立 Handler
         mHandler = new Handler();
-
-        // 藍芽訊息監控 Thread 初始化
-        receiveBlePost.start();
-        sendBlePost.start();
 
         // BlueTooth Adapter
         bluetoothManager = getSystemService(BluetoothManager.class);
@@ -106,6 +112,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 獲取按鈕的實例及監聽器
         Button btnBluetooth = binding.btnBluetooth;
         btnBluetooth.setOnClickListener(this);
+
+        //小指
+        binding.mSeekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bleSendText[1-1] = progress;
+                bleSendText[5] = 1; // 提示有訊息要發送
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        //名指
+        binding.mSeekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bleSendText[2-1] = progress;
+                bleSendText[5] = 1; // 提示有訊息要發送
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        //中指
+        binding.mSeekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bleSendText[3-1] = progress;
+                bleSendText[5] = 1; // 提示有訊息要發送
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        //食指
+        binding.mSeekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bleSendText[4-1] = progress;
+                bleSendText[5] = 1; // 提示有訊息要發送
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        //拇指
+        binding.mSeekBar5.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bleSendText[5-1] = progress;
+                bleSendText[5] = 1; // 提示有訊息要發送
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     @Override
@@ -114,6 +195,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             // 藍芽連線按鈕
             case R.id.btn_bluetooth:
+                if (connect){
+                    connect = false;
+                    isBlePostRunning = false; // 關閉藍芽訊息監控
+                    Toast.makeText(getApplicationContext(),"裝置連線中斷",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG + " BleConnect","裝置連線中斷");
+                    //binding.textBleStatus.setText("BlueTooth：Disconnect");
+                    // 清除藍芽設備資訊
+                    device = null;
+                    socket = null;
+                    is = null;
+                    os = null;
+                    sleep(1000); // 等待一會
+                }
                 blueToothStartUsing();
                 break;
         }
@@ -148,9 +242,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void run() {
             while (true)
-                while (isBlePostRunning && bleSendText[4] == 1){
+                while (isBlePostRunning && bleSendText[5] == 1){
                     sendMessage(formatIntArray(bleSendText));
-                    bleSendText[4] = 0;
+                    bleSendText[5] = 0;
                     sleep(50);
                 }
         }
@@ -188,42 +282,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, R.string.bluetooth_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }else{
-            // 檢查藍芽權限
-            if (checkBluetoothPermission()){
-                // 檢查設備是否支援低功耗藍芽(BLE)
-                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-                    Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
-                    finish();
-                }else{
-                    // 檢查是否開啟藍芽
-                    if (!bluetoothAdapter.isEnabled()) {
-                        // 彈出藍芽視窗(詢問是否打開藍芽)
-                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        enableBtIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
-                        startActivity(enableBtIntent);
-                    }else{
-                        // 開啟藍芽 Scanner 配對介面
-                        Toast.makeText(view.getContext(),"配對設備",Toast.LENGTH_SHORT).show();
-                        Intent bluetoothPicker = new Intent("android.bluetooth.devicepicker.action.LAUNCH");
-                        startActivity(bluetoothPicker);
-                    }
-                }
+            // 檢查設備是否支援低功耗藍芽(BLE)
+            if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+                finish();
             } else {
-                Toast.makeText(view.getContext(),"無藍芽權限",Toast.LENGTH_SHORT).show();
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // 权限未授予，请求权限
-                    //ActivityCompat.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_BLUETOOTH_SCAN_PERMISSION);
+                // 檢查權限
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+                }
+                // 檢查是否開啟藍芽
+                if (!bluetoothAdapter.isEnabled()) {
+                    // 彈出藍芽視窗(詢問是否打開藍芽)
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    enableBtIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                    startActivity(enableBtIntent);
                 } else {
-                    if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ) {
-                        String[] permissions = {Manifest.permission.BLUETOOTH_CONNECT};
-                        ActivityCompat.requestPermissions(this, permissions, BLUETOOTH_REQUEST_CODE);
-                        Log.d(TAG, "API31_正在請求藍芽權限");
-                    } else {
-                        requestPermissions(new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN},
-                                REQUEST_PERMISSION_BT);
-                        Log.d(TAG, "正在請求藍芽權限");
-                    }
+                    // 開啟藍芽 Scanner 配對介面
+                    Toast.makeText(view.getContext(), "配對設備", Toast.LENGTH_SHORT).show();
+                    Intent bluetoothPicker = new Intent("android.bluetooth.devicepicker.action.LAUNCH");
+                    startActivity(bluetoothPicker);
                 }
             }
         }
@@ -232,7 +310,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**藍芽廣播回傳**/
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
-            // 檢查GPS權限
+            // 檢查權限
             if ( ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                     || ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);
             String action = intent.getAction();
@@ -280,15 +358,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             is=socket.getInputStream(); // 輸出流
                             isBlePostRunning = true; // 啟動藍芽訊息接收監控器
                             connect = true; // 變更連線狀態
-                            mHandler.post(bleConnectionState); // 啟動藍芽連線狀態監控
-                            /*
+                            //mHandler.post(bleConnectionState); // 啟動藍芽連線狀態監控
+
+                            // 藍芽訊息監控 Thread 初始化
+                            receiveBlePost.start();
+                            sendBlePost.start();
+
+
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    
+                                    binding.mSeekBar1.setProgress(0);
+                                    binding.mSeekBar2.setProgress(0);
+                                    binding.mSeekBar3.setProgress(0);
+                                    binding.mSeekBar4.setProgress(0);
+                                    binding.mSeekBar5.setProgress(0);
                                 }
                             });
-                            */
+
                         }
                     }
                     catch (IOException e) {
@@ -358,16 +445,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    /**確認藍芽權限**/
-    private boolean checkBluetoothPermission() {
-        if (checkSelfPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
-                == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     /**將陣列轉為字串**/
     private static String formatIntArray(int[] intArray) {
         StringBuilder sb = new StringBuilder();
@@ -379,6 +456,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             sb.append(String.format("%0" + length + "d", intArray[i]));
         }
         return sb.toString();
+    }
+
+    /**決定字串每個元素的長度**/
+    private static int getLength(int index) {
+        switch (index) {
+            case 5:
+                return 1;
+            default:
+                return 3;
+        }
     }
 
     /**隱藏導航欄**/
